9c14f37489f180fcf414bc3d67878f89dd3044de
M R JEEVAN <43513074+mrjeevan@users.noreply.github.com>
2020-04-25 12:53:02 +0530

    Count of (number of unique char) in given str

--- Char_Count.py
+++ Char_Count.py
@@ -1,8 +1,8 @@
-def multiple_letter_count(a):
+def multiple_letter_count(input):
     b = {a[i]:a.count(a[i]) for i in range(0,len(a))}
     return b
     # return b
 
 
-j = input(" Enter the string : ")
-print(multiple_letter_count(j))
+str = input(" Enter the string : ")
+print(multiple_letter_count(str))
